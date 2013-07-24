d349293e0a308d72a1b58267fcd63fcd49213e7a
AbhimanyuC <life.abhi.life@gmail.com>
2013-07-25 01:02:10 +0530

    edited spider

--- ScrapyPart/project2/project2/spiders/project2spider.py
+++ ScrapyPart/project2/project2/spiders/project2spider.py
@@ -2,42 +2,38 @@ from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from project2.items import Project2Item
 #import MySQLdb
+from scrapy.http import Request
 
 class ProjectSpider(BaseSpider):
     name = "project2spider"
     allowed_domains = ["http://directory.thesun.co.uk/"]
+    current_page_no = 1
+    start_urls = [
+        'http://directory.thesun.co.uk/find/uk/computer-repair'
+        ]
+
+    def get_next_url(self, fired_url):
+        if '/page/' in fired_url:
+            url, page_no = fired_url.rsplit('/page/', 1)
+        else:
+            if self.current_page_no != 1:
+                #end of scroll
+                return 
+        self.current_page_no += 1
+        return "http://directory.thesun.co.uk/find/uk/computer-repair/page/%s" % self.current_page_no
 
 
     
-    # ""**** I tried the below code to crawl pages also , BUT I was not sure abt it, so I used the hardcore values for all the pages***""
-
-
-    #start_urls = [
-     #   "http://directory.thesun.co.uk/find/uk/computer-repair"
-    #]
-    #next_urls = ['http://directory.thesun.co.uk/find/uk/computer-repair/page/%s' % page for page in xrange(2, 11)]
-    #start_urls.extend(next_urls)
-     
-    start_urls = [
-        'http://directory.thesun.co.uk/find/uk/computer-repair',
-        'http://directory.thesun.co.uk/find/uk/computer-repair/page/2',
-        'http://directory.thesun.co.uk/find/uk/computer-repair/page/3',
-        'http://directory.thesun.co.uk/find/uk/computer-repair/page/4',
-        'http://directory.thesun.co.uk/find/uk/computer-repair/page/5',
-        'http://directory.thesun.co.uk/find/uk/computer-repair/page/6',
-        'http://directory.thesun.co.uk/find/uk/computer-repair/page/7',
-        'http://directory.thesun.co.uk/find/uk/computer-repair/page/8',
-        'http://directory.thesun.co.uk/find/uk/computer-repair/page/9',
-        'http://directory.thesun.co.uk/find/uk/computer-repair/page/10'
-        ]
+    
     
 
     
 # the parse procedure, and here is the codes which declares which field to scrap. 
     def parse(self, response):
+        fired_url = response.url
         hxs = HtmlXPathSelector(response)
         sites = hxs.select('//div[@class="abTbl "]')
-        items = []
+        #items = []
         for site in sites:
             item = Project2Item()
             item['Catogory'] = site.select('span[@class="icListBusType"]/text()').extract()
@@ -48,7 +44,11 @@ class ProjectSpider(BaseSpider):
             item['adress_name'] = site.select('span[@class="searchInfoLabel"]/span/text()').extract()
             item['Photo_name'] = site.select('img/@alt').extract()
             item['Photo_path'] = site.select('img/@src').extract()
-            items.append(item)
-        return items
+            #items.append(item)
+            yield item
+        next_url = self.get_next_url(fired_url)
+        if next_url:
+            yield Request(next_url, self.parse, dont_filter=True)
+        #return items
 
  
