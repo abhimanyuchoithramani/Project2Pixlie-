3ed3b6076d9c4282a4062f7c96f9a2465577f327
AbhimanyuC <life.abhi.life@gmail.com>
2013-07-25 09:56:48 +0530

    depth limmit set as 20 in settings

--- ScrapyPart/project2/project2/settings.py
+++ ScrapyPart/project2/project2/settings.py
@@ -9,6 +9,9 @@ BOT_NAME = 'project2'
 SPIDER_MODULES = ['project2.spiders']
 NEWSPIDER_MODULE = 'project2.spiders'
 
+#this sets the crawling depth, here I am setting as 20 pages.
+DEPTH_LIMIT = 20
+
 ITEM_PIPELINES = (
     'project2.pipelines.MySQLStorePipeline',
 )
